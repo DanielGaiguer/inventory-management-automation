8c45a202e0c431899f3a53fad6ab0665fa0d1271
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-01-14 10:42:12 -0300

    add feat for schema

--- src/_lib/prisma.ts
+++ src/_lib/prisma.ts
@@ -2,9 +2,23 @@ import "dotenv/config";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "../generated/prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = process.env.DATABASE_URL || "file:./dev.db";
 
+// Criar o adapter
 const adapter = new PrismaBetterSqlite3({ url: connectionString });
-const prisma = new PrismaClient({ adapter });
 
-export { prisma };
+// Criar uma instância de PrismaClient
+// Evitar múltiplas instâncias em dev (hot reload)
+const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+    log: ["query"], // log das queries
+  });
+
+// Salvar no global para hot reload em dev
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
