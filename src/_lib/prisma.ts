a13cfbb0a90f090fc01b44a1692aeea4841370d7
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-01-16 08:19:23 -0300

    version based for prisma

--- src/_lib/prisma.ts
+++ src/_lib/prisma.ts
@@ -1,15 +1,27 @@
+// Este arquivo ele garante que apenas 1 intancia do prisma client, ira ser instanciada no Next.
+// Se voce nao tiver este codigo, a cada recompilação do Next ele vai abrir uma nova conexao com o banco
+// Com este arquivo você garante que quando você recompilar esta aplicação, você não vai abrir multiplas conexões com seu banco de dados
 import { PrismaClient } from "@prisma/client"
+import { PrismaPg } from "@prisma/adapter-pg"
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined
+declare global {
+  // eslint-disable-next-line no-var
+  var cachedPrisma: PrismaClient
 }
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["query"], // opcional, útil para debug
-  })
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+})
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma
+let prisma: PrismaClient
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient({ adapter })
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient({ adapter })
+  }
+  prisma = global.cachedPrisma
 }
+
+export const db = prisma
